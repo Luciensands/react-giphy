a8068b8f41762ef988fe12697a46cab724c3f0b6
Joseph Sandoval <joesando@gmail.com>
2023-03-14 21:41:03 +0900

    added onclick function

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,34 @@
 import '../assets/stylesheets/application.scss';
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
+
+  render () {
+    return (
+      <div className={this.state.clicked ? 'clicked' : null}
+      onClick={this.handleClick}>
+       Hello, {this.props.name}!
+       </div>
+    )
+  }
+}
+const root = document.getElementById('root')
+if (root) {
+  ReactDOM.render(
+    <Hello name="Joe" />,
+    root
+  );
+}
